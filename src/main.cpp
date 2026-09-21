ac1aea2207c5d5e333e04a943248ce750b2b25f3
Samuel Reig Bolaños <samuelreig@gmail.com>
2026-09-20 22:28:53 -0600

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+#define Pot 34
+#define LED 4
+#define Button 0
+#define PWM 0
+#define res 3
+
+void setup() 
+{
+  pinMode(LED, OUTPUT);
+  pinMode(Button, INPUT_PULLUP);
+  Serial.begin(9600);
+  analogReadResolution(res);
+  ledcSetup(PWM, 300, res);
+  ledcAttachPin(LED, PWM);
+}
+
+void loop() 
+{
+  int valor_pot = analogRead(Pot);
+  int volt_pot = analogReadMilliVolts(Pot);
+  ledcWrite(0, 8);
+  Serial.printf("Numerico: %d | Voltaje: %d\n", valor_pot, volt_pot);
+  delay(400);
+}
+
